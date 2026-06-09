25eaddf8c034f36bd0e4b14c823d6f1238d4dd9c
Draven Nova <draven77medina@hotmail.com>
2026-06-08 22:32:38 -0700

    Initial Commit

---
+++ ActivityApp/ActivityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityAppApp.swift
+//  ActivityApp
+//
+//  Created by Draven Nova on 6/8/26.
+//
+
+import SwiftUI
+
+@main
+struct ActivityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActivityAppTests/ActivityAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  ActivityAppTests.swift
+//  ActivityAppTests
+//
+//  Created by Draven Nova on 6/8/26.
+//
+
+import Testing
+@testable import ActivityApp
+
+struct ActivityAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
